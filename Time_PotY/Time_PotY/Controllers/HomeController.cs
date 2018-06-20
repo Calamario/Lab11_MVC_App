a64d6cd06f70d28ab9b2769473e993b3bbf28526
Calamario <mario.nishio@gmail.com>
2018-06-19 23:59:42 -0700

    initial commit

---
+++ Time_PotY/Time_PotY/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Time_PotY.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int startYear, int endYear)
+        {
+            return RedirectToAction("Results", new { startYear, endYear });
+        }
+
+        public IActionResult Results(int startYear, int endYear)
+        {
+            return RedirectToAction("ResponseExample", new):
+        }
+
+        public IActionResult ResponseExample()
+        {
+
+        }
+    }
+}
